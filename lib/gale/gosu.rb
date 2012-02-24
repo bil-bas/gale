f6284625f433a29a17ccad592877bc4a061848b2
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-02-24 14:48:22 +0000

    Allowed user to specify a Gosu window to use.

--- lib/gale/gosu.rb
+++ lib/gale/gosu.rb
@@ -19,14 +19,21 @@ end
 
 module Gale
   class File
-    # @option :column [Integer] (Float::INFINITY) Max number of columns to use.
+    # Create a sprite-sheet from the frames in the File. By default, this will be a horizontal strip.
+    #
+    # @option :columns [Integer] (#size) Number of columns to use. Will leave excess columns empty.
+    # @option :window [Gosu::Window] ($window) Window used to create the image.
     # @return Gosu::Image
     def to_spritesheet(options = {})
-      columns = options[:columns] || (defined?(Float::INFINITY) ? Float::INFINITY : 1_000_000)
-      columns = [columns, size].min
+      options = {
+          :columns => size,
+          :window => $window,
+      }.merge! options
+
+      columns = options[:columns]
       rows = size.fdiv(columns).ceil
 
-      sheet = TexPlay.create_image $window, columns * width, rows * height, :caching => true
+      sheet = TexPlay.create_image options[:window], columns * width, rows * height, :caching => true
       each do |frame|
         row, column = frame.index.divmod columns
         sheet.splice frame.to_image, column * width, row * height
@@ -36,8 +43,13 @@ module Gale
   end
 
   class Frame
+    # @option :window [Gosu::Window] ($window) Window used to create the image.
     # @return Gosu::Image
-    def to_image
+    def to_image(options = {})
+      options = {
+          :window => $window,
+      }.merge! options
+
       # Hack because I have no idea how to make #to_blob properly.
       file = Tempfile.new 'gale_bitmap'
       image = nil
@@ -45,7 +57,7 @@ module Gale
         file.close # Don't actually use it directly, since we are going to overwrite it.
         export_bitmap file.path
 
-        image = Gosu::Image.new $window, file.path, :caching => true
+        image = Gosu::Image.new options[:window], file.path, :caching => true
         if transparent_color?
           image.clear :dest_select => Gosu::Color.from_gale(transparent_color), :tolerance => 0.001
         end
@@ -58,8 +70,13 @@ module Gale
   end
 
   class Layer
+    # @option :window [Gosu::Window] ($window) Window used to create the image.
     # @return Gosu::Image
-    def to_image
+    def to_image(options = {})
+      options = {
+          :window => $window,
+      }.merge! options
+
       # Hack because I have no idea how to make #to_blob properly.
       file = Tempfile.new 'gale_bitmap'
       image = nil
@@ -67,7 +84,7 @@ module Gale
         file.close # Don't actually use it directly, since we are going to overwrite it.
         export_bitmap file.path
 
-        image = Gosu::Image.new $window, file.path, :caching => true
+        image = Gosu::Image.new options[:window], file.path, :caching => true
         if transparent_color?
           image.clear :dest_select => Gosu::Color.from_gale(transparent_color), :tolerance => 0.001
         end
