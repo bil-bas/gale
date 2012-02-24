85733d34f3e7dcfe70c02d58e05a468a63c5492c
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-02-24 11:36:23 +0000

    Removed unused code.

--- lib/gale/dll.rb
+++ lib/gale/dll.rb
@@ -3,35 +3,6 @@ require 'ffi'
 ENV['PATH'] = File.expand_path("../../../bin", __FILE__) + File::PATH_SEPARATOR + ENV['PATH']
 
 module Gale
-
-  module GDIPlus
-    extend FFI::Library
-    ffi_lib 'gdiplus'
-    ffi_convention :stdcall
-
-    # GpStatus WINGDIPAPI
-    # GdipCreateBitmapFromHBITMAP(HBITMAP hbm, HPALETTE hpal, GpBitmap** bitmap);
-    #
-    attach_function :bitmap_from_hbitmap, :GdipCreateBitmapFromHBITMAP, [:pointer, :pointer, :pointer], :long
-
-  end
-
-  module GDI32
-    extend FFI::Library
-    ffi_lib 'gdi32'
-    ffi_convention :stdcall
-    # int GetDIBits(
-    #     __in     HDC hdc,
-    #     __in     HBITMAP hbmp,
-    #     __in     UINT uStartScan,
-    #     __in     UINT cScanLines,
-    #     __out    LPVOID lpvBits,
-    #     __inout  LPBITMAPINFO lpbi,
-    #     __in     UINT uUsage
-    # );
-    attach_function :bitmap_to_blob, :GetDIBits, [:pointer, :pointer, :uint, :uint, :buffer_out, :pointer, :uint], :long
-  end
-
   # Directly exposes galefile.dll to Ruby
   module Dll
     extend FFI::Library
@@ -58,12 +29,14 @@ module Gale
       WIDTH = 5                 # Return value is width of image by pixel.
       HEIGHT = 6                # Return value is height of image by pixel.
     end
-    
+
+    # Passed to #frame_info (ggGetFrameInfo)
     module FrameInfo
       TRANSPARENT_COLOR = 1 # Return value is the transparent color.
       DELAY_MS = 2 # Return value is the delay by milli-second.
       DISPOSAL = 3 # Return value is the disposal type after display.
-      
+
+      # Returned by #frame_info (ggGetFrameInfo)
       module Disposal
         NOT_SPECIFIED = 0 # Not specified.
         NOT_DISPOSED = 1 # Not disposed.
@@ -72,30 +45,12 @@ module Gale
       end
     end
 
+    # Passed to #layer_info (ggGetLayerInfo)
     module LayerInfo
       VISIBLE = 1           # If the layer is visible, return value is 1.
       TRANSPARENT_COLOR = 2 # Return value is the transparent color.
       OPACITY = 3           # Return value is the opacity(0~255).
       ALPHA_CHANNEL = 4     # If the alpha-channel is effective, return value is 1
-    end
-    
-    # typedef struct tagBITMAP {
-    #   LONG   bmType;
-    #   LONG   bmWidth;
-    #   LONG   bmHeight;
-    #   LONG   bmWidthBytes;
-    #   WORD   bmPlanes;
-    #   WORD   bmBitsPixel;
-    #   LPVOID bmBits;
-    # } BITMAP, *PBITMAP;
-    class HBITMAP < FFI::Struct
-      layout :type,           :long,
-             :width,          :long,
-             :height,         :long,
-             :width_bytes,    :long,
-             :planes,         :uint16,
-             :bits_per_pixel, :uint16,
-             :bits,           :pointer
     end
 
     # LPVOID __stdcall ggOpen(LPCSTR apath);
@@ -218,71 +173,67 @@ module Gale
     # Return:     If the function succeeds, the return value is the handle of
     # bitmap.
     # If the function fails, the return value is 0.
-    attach_function :bitmap, :ggGetBitmap, [:pointer, :long, :long], :pointer
+    #attach_function :bitmap, :ggGetBitmap, [:pointer, :long, :long], :pointer
 
-=begin
-HBITMAP __stdcall ggGetAlphaChannel(LPVOID pFile,LONG frameNo,LONG layerNo);
-  Contents:   Retrieves the handle of bitmap of alpha channel of specified
-              frame and layer. The handle must not be deleted.
-  Parameters: pFile = The address of the gale object.
-              frameNo = The frame index which begin from zero.
-              layerNo = The layer index which begin from zero.
-  Return:     If the function succeeds, the return value is the handle of
-              bitmap.
-              If the function fails, the return value is 0.
-=end
-    attach_function :alpha_channel, :ggGetAlphaChannel, [:pointer, :long, :long], :pointer
 
-=begin
-HPALETTE __stdcall ggGetPalette(LPVOID pFile,LONG frameNo);
-  Contents:   Retrieves the handle of palette of specified frame.
-              The handle must not be deleted.
-  Parameters: pFile = The address of the gale object.
-              frameNo = The frame index which begin from zero.
-  Return:     If the function succeeds, the return value is the handle of
-              palette.
-              If the function fails, the return value is 0.
-=end
-    attach_function :palette, :ggGetPalette, [:pointer, :long], :pointer
-=begin
-LONG __stdcall ggDrawBitmap(LPVOID pFile,LONG frameNo,LONG layerNo,HDC toDC,LONG toX,LONG toY);
-  Contents:   Draws the image of specified frame and layer to specified
-              device context.
-  Parameters: pFile = The address of the gale object.
-              frameNo = The frame index which begin from zero.
-              layerNo = The layer index which begin from zero.
-                        If it is -1, combined image is retrieved.
-              toDC = The handle of device context of the destination.
-              toX = Specifies the x-coordinate of the destination.
-              toY = Specifies the y-coordinate of the destination.
-  Return:     If the function succeeds, the return value is 1.
-              If the function fails, the return value is 0.
+    #HBITMAP __stdcall ggGetAlphaChannel(LPVOID pFile,LONG frameNo,LONG layerNo);
+    #  Contents:   Retrieves the handle of bitmap of alpha channel of specified
+    #              frame and layer. The handle must not be deleted.
+    #  Parameters: pFile = The address of the gale object.
+    #              frameNo = The frame index which begin from zero.
+    #              layerNo = The layer index which begin from zero.
+    #  Return:     If the function succeeds, the return value is the handle of
+    #              bitmap.
+    #              If the function fails, the return value is 0.
+    #attach_function :alpha_channel, :ggGetAlphaChannel, [:pointer, :long, :long], :pointer
 
-LONG __stdcall ggExportBitmap(LPVOID pFile,LONG frameNo,LONG layerNo,LPCSTR outPath);
-  Contents:   Creates a bmp file from the image of specified frame and
-              layer.
-  Parameters: pFile = The address of the gale object.
-              frameNo = The frame index which begin from zero.
-              layerNo = The layer index which begin from zero.
-                        If it is -1, combined image is created.
-              outPath = The filename for output.
-  Return:     If the function succeeds, the return value is 1.
-              If the function fails, the return value is 0.
-=end
+
+    #HPALETTE __stdcall ggGetPalette(LPVOID pFile,LONG frameNo);
+    #  Contents:   Retrieves the handle of palette of specified frame.
+    #              The handle must not be deleted.
+    #  Parameters: pFile = The address of the gale object.
+    #              frameNo = The frame index which begin from zero.
+    #  Return:     If the function succeeds, the return value is the handle of
+    #              palette.
+    #              If the function fails, the return value is 0.
+    #attach_function :palette, :ggGetPalette, [:pointer, :long], :pointer
+
+
+    #LONG __stdcall ggDrawBitmap(LPVOID pFile,LONG frameNo,LONG layerNo,HDC toDC,LONG toX,LONG toY);
+    #  Contents:   Draws the image of specified frame and layer to specified
+    #              device context.
+    #  Parameters: pFile = The address of the gale object.
+    #              frameNo = The frame index which begin from zero.
+    #              layerNo = The layer index which begin from zero.
+    #                        If it is -1, combined image is retrieved.
+    #              toDC = The handle of device context of the destination.
+    #              toX = Specifies the x-coordinate of the destination.
+    #              toY = Specifies the y-coordinate of the destination.
+    #  Return:     If the function succeeds, the return value is 1.
+    #              If the function fails, the return value is 0.
+
+    #LONG __stdcall ggExportBitmap(LPVOID pFile,LONG frameNo,LONG layerNo,LPCSTR outPath);
+    #  Contents:   Creates a bmp file from the image of specified frame and
+    #              layer.
+    #  Parameters: pFile = The address of the gale object.
+    #              frameNo = The frame index which begin from zero.
+    #              layerNo = The layer index which begin from zero.
+    #                        If it is -1, combined image is created.
+    #              outPath = The filename for output.
+    #  Return:     If the function succeeds, the return value is 1.
+    #              If the function fails, the return value is 0.
     attach_function :export_bitmap, :ggExportBitmap, [:pointer, :long, :long, :string], :long
     
-=begin
-LONG __stdcall ggExportAlphaChannel(LPVOID pFile,LONG frameNo,LONG layerNo,LPCSTR outPath);
-  Contents:   Creates a bmp file from the alpha channel of specified frame
-              and layer.
-  Parameters: pFile = The address of the gale object.
-              frameNo = The frame index which begin from zero.
-              layerNo = The layer index which begin from zero.
-              outPath = The filename for output.
-  Return:     If the function succeeds, the return value is 1.
-              If the function fails, the return value is 0.
-=end
 
+    #LONG __stdcall ggExportAlphaChannel(LPVOID pFile,LONG frameNo,LONG layerNo,LPCSTR outPath);
+    #  Contents:   Creates a bmp file from the alpha channel of specified frame
+    #              and layer.
+    #  Parameters: pFile = The address of the gale object.
+    #              frameNo = The frame index which begin from zero.
+    #              layerNo = The layer index which begin from zero.
+    #              outPath = The filename for output.
+    #  Return:     If the function succeeds, the return value is 1.
+    #              If the function fails, the return value is 0.
     attach_function :export_alpha_channel, :ggExportAlphaChannel, [:pointer, :long, :long, :string], :long
   end
 end
